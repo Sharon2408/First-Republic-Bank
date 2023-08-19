5e115cfcb6e8973fea31f623f578c83d04f13116
SivaBharat <sivabharthece@gmail.com>
2023-08-19 14:42:09 +0530

    .

--- src/app/user/signup/signup.component.ts
+++ src/app/user/signup/signup.component.ts
@@ -1,37 +1,44 @@
-import { Component } from '@angular/core';
-import { FormBuilder, Validators,FormControl } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css'],
+  styleUrls: ['./signup.component.css']
 })
-export class SignupComponent {
-firstName!:FormControl;
+export class SignupFormComponent implements OnInit {
+  personalInfoForm: FormGroup;
+  contactInfoForm: FormGroup;
+  accountInfoForm: FormGroup;
 
+  constructor(private formBuilder: FormBuilder) {}
 
+  ngOnInit(): void {
+    this.personalInfoForm = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      gender: ['', Validators.required],
+      maritalStatus: ['', Validators.required]
+    });
 
-  firstFormGroup = this._formBuilder.group({
-    firstName: ['', Validators.required],
-    lastName: ['', Validators.required],
-    gender: ['', Validators.required],
-    maritalStatus: ['', Validators.required],
-    email: ['', Validators.required],
-  });
+    this.contactInfoForm = this.formBuilder.group({
+      address: ['', Validators.required],
+      pincode: ['', Validators.required],
+      aadharNo: ['', Validators.required],
+      panNo: ['', Validators.required]
+    });
 
-  secondFormGroup = this._formBuilder.group({
-    address: ['', Validators.required],
-    pincode: ['', Validators.required],
-    adharNumber: ['', Validators.required],
-    panNumber: ['', Validators.required],
-    phoneNumber: ['', Validators.required],
-  });
+    this.accountInfoForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      phoneNo: ['', Validators.required],
+      userId: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
 
-  thirdFormGroup = this._formBuilder.group({
-    userId: ['', Validators.required],
-    password: ['', Validators.required],
-    confirmPassword: ['', Validators.required],
-  });
-
-  constructor(private _formBuilder: FormBuilder) {}
+  submitForm(): void {
+    if (this.personalInfoForm.valid && this.contactInfoForm.valid && this.accountInfoForm.valid) {     
+      console.log('Form submitted successfully');
+    }
+  }
 }
