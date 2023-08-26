cbf12606d8052af7ac0021dd3e344fd68b7a4098
Amigosouls <amigosouls022@gmail.com>
2023-08-26 16:48:48 +0530

    complete

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,9 +1,7 @@
 export const environment={
     calculationType:"http://localhost:3000/calculationType",
     userDetails:"http://localhost:3000/userDetails",
-<<<<<<< HEAD
-    navbar:"http://localhost:3000/NavBar"
-=======
+    navbar:"http://localhost:3000/NavBar",
     sessionDetails:"http://localhost:3000/session"
->>>>>>> e6e07e75887a0c687523ca2eabc3c9fe87dcbd75
+
 }
