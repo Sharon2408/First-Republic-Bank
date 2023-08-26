7b556e04148f67f0fb36f83b56b3bfa83dc3e13d
Amigosouls <amigosouls022@gmail.com>
2023-08-26 16:39:57 +0530

    navbar completed

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,4 +1,5 @@
 export const environment={
     calculationType:"http://localhost:3000/calculationType",
     userDetails:"http://localhost:3000/userDetails",
+    navbar:"http://localhost:3000/NavBar"
 }
---
+++ src/models/navContent.ts
@@ -0,0 +1,8 @@
+export interface NavModel{
+    id:number,
+    label:string,
+    name:string,
+    img:string,
+    about:string,
+    description:Array<string>
+}
