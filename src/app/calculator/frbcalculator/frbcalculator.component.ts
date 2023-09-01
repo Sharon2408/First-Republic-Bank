3b3c5da33b8f4f15a8b4a0735d3bdad9eb4c73b4
hemanth04/09 <hemanthbalakrishnan2002@gmail.com>
2023-09-01 14:22:26 +0530

    commit in cal

--- src/app/calculator/frbcalculator/frbcalculator.component.ts
+++ src/app/calculator/frbcalculator/frbcalculator.component.ts
@@ -23,19 +23,25 @@ export class FrbcalculatorComponent implements OnInit {
   p!: number;
   n!: number;
   r!: number;
+  R!:number;
   pow!: number;
   EMIAmount: number = 8514.99;
+  Emi2!:number;
+  num:number=1;
+  GSTAmount:number=100000;
   Calculatorname!: calculationType;
   EMICalculator() {
     this.p = this.loanAmount;
     this.n = this.Year * 12;
-    this.r = this.InterestRate / 12 / 100;
-    this.pow = Math.pow((1 + this.r), this.n)
+   this.num=1;
+    this.r = (this.InterestRate / 12 / 100);
+    this.R=(this.num+this.r);
+    this.pow = Math.pow(this.R,this.n)
     this.EMIAmount = this.p * this.r * this.pow / this.pow - 1;
-
-
-    console.log("power"+this.pow);
-  }
+//this.Emi2=this.p*this.r*Math.pow((1+this.r),this.n)/(Math.pow((1+this.r),this.n)-1)
+// console.log("R"+this.r)
+//     console.log("power"+this.pow);
+     }
   ngOnInit(): void {
     this.ct.fetchCalDetails().subscribe((res) => {
       this.type = res;
